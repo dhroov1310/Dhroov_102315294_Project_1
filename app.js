55d56e7ec4a9e48da036fe2fe06c034add59cd8f
Dhroov <dhroov1310@gmail.com>
2026-02-21 19:44:11 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,8 @@
-// ===== Cognito config =====
 const cognitoDomain = "https://us-east-18y4bjwsud.auth.us-east-1.amazoncognito.com";
 const clientId = "77h22id44pgblfeigt5q10pilj";
 const redirectUri = "https://dhroov1310.github.io/Dhroov_102315294_Project_1/";
 
-// ===== Buttons =====
-const loginBtn = document.getElementById("loginBtn");
-const logoutBtn = document.getElementById("logoutBtn");
-
-// ===== Login =====
-loginBtn.onclick = () => {
+document.getElementById("loginBtn").onclick = () => {
   const loginUrl =
     `${cognitoDomain}/login?client_id=${clientId}` +
     `&response_type=token` +
@@ -18,19 +12,10 @@ loginBtn.onclick = () => {
   window.location.href = loginUrl;
 };
 
-// ===== Logout =====
-logoutBtn.onclick = () => {
+document.getElementById("logoutBtn").onclick = () => {
   const logoutUrl =
     `${cognitoDomain}/logout?client_id=${clientId}` +
     `&logout_uri=${encodeURIComponent(redirectUri)}`;
 
   window.location.href = logoutUrl;
-};
-
-// ===== Detect login return =====
-window.onload = () => {
-  if (window.location.hash.includes("id_token")) {
-    console.log("User logged in");
-    alert("Login successful ✅");
-  }
 };
